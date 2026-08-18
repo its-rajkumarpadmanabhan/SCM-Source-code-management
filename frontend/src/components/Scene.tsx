7c513c3e94d8c2d262e221fa36d477a26782ee1d
RajkumarPadmanabhan <rajkumarpadmanabhan23@gmail.com>
2026-08-18 18:00:36 +0530

    Commit 5: Create a rotating 3D element

--- frontend/src/components/Scene.tsx
+++ frontend/src/components/Scene.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+function Cube() {
+  const mesh = useRef<any>();
+  useFrame(() => {
+    if (mesh.current) {
+      mesh.current.rotation.x += 0.01;
+      mesh.current.rotation.y += 0.01;
+    }
+  });
+  return (
+    <mesh ref={mesh}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="hotpink" />
+    </mesh>
+  );
+}
 
 export default function Scene() {
   return (
     <div className="absolute inset-0 -z-10">
       <Canvas>
+        <Cube />
       </Canvas>
     </div>
   );
